d970f851e824ce24d39e12ca35a962057cb03556
programmerBrandon <100105534+programmerBrandon@users.noreply.github.com>
2023-11-04 17:04:02 -0400

    Update gui.py - Added ability to show data by attack type

--- gui.py
+++ gui.py
@@ -2,21 +2,16 @@ import streamlit as st
 import pandas as pd
 import streamlit.components.v1 as components
 
-country_codes = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/country_codes.csv")
-country_indicators = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/country_indicators.csv")
-pirate_attacks = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/pirate_attacks.csv")
+country_codes = pd.read_csv('https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/country_codes.csv')
+country_indicators = pd.read_csv('https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/country_indicators.csv')
+pirate_attacks = pd.read_csv('https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/pirate_attacks.csv')
 
-
-#components.html(
-##    """<link rel="stylesheet" href="pyrates.css">
-#    """
-#)
-
-st.title ="Pyrates"
-st.header = "Welcome to the Pyrates App!"
+st.title ='Pyrates'
+st.header = 'Welcome to the Pyrates App!'
 
 st.dataframe(country_codes)
 
+#GET PIRATE ATTACKS BY NEAREST COUNTRY - CODE BY AM
 cnames = country_codes[['country_name']]
 option = st.selectbox(
     'Select A Country To View Pirate Attacks for that country:',
@@ -24,4 +19,14 @@ option = st.selectbox(
 
 st.write('You selected:', option)
 
+#SELECT country_name WHERE country_name IS option
 country_codes[country_codes['country_name'] == option]
+
+#GET PIRATE ATTACKS BY ATTACK TYPE - BG
+def attackTypeData():
+    selected_option = st.selectbox('Select Attack Type:', ('N/A','Attempted', 'Boarded', 'Hijacked'))
+
+    if selected_option is not 'N/A':
+        pirate_attacks[pirate_attacks['attack_type'] == selected_option] 
+
+attackTypeData()
