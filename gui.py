f17feeb84265f195416918fa13d4c102be88ffd4
programmerBrandon <100105534+programmerBrandon@users.noreply.github.com>
2023-11-04 18:52:19 -0400

    Update gui.py - Reformatted code to make code structure better.
    Further improvements will be made as we attempt to fix country selection bug.

--- gui.py
+++ gui.py
@@ -2,42 +2,35 @@ import streamlit as st
 import pandas as pd
 import streamlit.components.v1 as components
 
+#Create data frames for each CSV file.
 country_codes = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/country_codes.csv")
 country_indicators = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/country_indicators.csv")
 pirate_attacks = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/pirate_attacks.csv")
 
-
-#components.html(
-##    """<link rel="stylesheet" href="pyrates.css">
-#    """
-#)
-
+# Helper function for select__by_years() function.
 def years_only(x):
     return x.split('-')[0]
 
-# select by year function creates a selectbox that allows the user to select a year to filter attacks by
+# Select by year function creates a selectbox that allows the user to get attacks by year. Code by SG
 def select_by_year(df):
     df['date'] = df['date'].apply(years_only)
     year = st.selectbox('Select A Year To View Pirate Attacks for that Year:', df["date"].unique())
     st.write('You selected:', df.loc[df['date'] == year])
 
+# Function that creates selectbox that allows user to get attacks by type. Code by SG & BG. 
 def select_by_attack(df):
     attack = st.selectbox('Select An Attack Type to View Attacks of that Type:', df["attack_type"].dropna().unique())
     st.write('You selected:', df.loc[df['attack_type'] == attack])
 
-st.title ="Pyrates"
-st.header = "Welcome to the Pyrates App!"
-
-st.dataframe(country_codes)
+#st.dataframe(country_codes)
 
 cnames = country_codes['country_name']
-st.dataframe(country_indicators)
-st.dataframe(pirate_attacks)
+#st.dataframe(country_indicators)
+#st.dataframe(pirate_attacks)
 option = st.selectbox(
     'Select A Country To view Info:',
     (cnames))
 
-#calling the select by year function 
+##### MAIN DRIVER CODE ###
 select_by_year(pirate_attacks)
-#calling the select by attack function
 select_by_attack(pirate_attacks)
