47c71b8e8edc03e592002079fcb2e2de167d242b
Amrit Madabushi <am2699@njit.edu>
2023-11-04 21:25:12 -0400

    AM gui.py map update
    added map

--- gui.py
+++ gui.py
@@ -2,38 +2,73 @@ import streamlit as st
 import pandas as pd
 import streamlit.components.v1 as components
 
+
 country_codes = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/country_codes.csv")
 country_indicators = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/country_indicators.csv")
 pirate_attacks = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/pirate_attacks.csv")
+temp = country_codes
+country_dict = temp.set_index('country_name').T.to_dict('list')
 
-
+st.title('Pyrates')
+st.header('Welcome to the Pyrates App!')
 #components.html(
 ##    """<link rel="stylesheet" href="pyrates.css">
 #    """
 #)
 
-st.title ="Pyrates"
-st.header = "Welcome to the Pyrates App!"
+def years_only(x):
+    return x.split('-')[0]
 
-#displays the entire csv file
+# select by year function creates a selectbox that allows the user to select a year to filter attacks by
+def select_by_year(df):
+    df['date'] = df['date'].apply(years_only)
+    year = st.selectbox('Select A Year To View Pirate Attacks for that Year:', df["date"].unique())
+    st.write('You selected:', df.loc[df['date'] == year])
+
+def select_by_attack(df):
+    attack = st.selectbox('Select An Attack Type to View Attacks of that Type:', df["attack_type"].dropna().unique())
+    st.write('You selected:', df.loc[df['attack_type'] == attack])
+
+def select_by_country(df):
+    country = st.selectbox('Select A Country To View Pirate Attacks for that Country:', country_codes['country_name'])
+    st.write("You selected:", df.loc[df['nearest_country'] == country_dict[country][0]])
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+
+
 st.dataframe(country_codes)
 
-#Names of the countries
-cnames = country_codes[['country_name']]
+#cnames = country_codes['country_name']
 st.dataframe(country_indicators)
 st.dataframe(pirate_attacks)
-option = st.selectbox(
-    'Select A Country To view Info:',
-    (cnames))
+# option = st.selectbox(
+#   'Select A Country To view Info:',
+#    (cnames))
 
-st.write('You selected:', option)
 
-country_codes[country_codes['country_name'] == option]
+#calling the select by year function 
+select_by_year(pirate_attacks)
+#calling the select by attack function
+select_by_attack(pirate_attacks)
+#calling the select by country function
+select_by_country(pirate_attacks)
 
-#represents the value of the country code from the country name
-code = country_codes.loc[country_codes['country_name'] == option, ['country']].values[0]
-country_codes.loc[country_codes.at['country_name'] == option, ['country']][0]
-#country_codes.at[option, 'country']
-#country_codes[['country']]
-#pirate_attacks[['nearest_country']]
-#pirate_attacks.loc[pirate_attacks['nearest_country'] == code]
+DATE_COLUMN = 'date'
+DATA_URL = 'https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/pirate_attacks.csv'
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache_data)")
+
+st.subheader('Map of all pirate attacks')
+st.map(data)
