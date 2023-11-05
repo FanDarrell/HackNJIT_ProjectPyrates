7d8d20f2cf6c330fc00c6e262d48068c08379951
programmerBrandon <100105534+programmerBrandon@users.noreply.github.com>
2023-11-05 01:29:42 -0400

    Update gui.py - Redid the GUI to use a tabbed format
    Redid the GUI to use a tabbed format that divides up the 3 major features of the app in an easy to use and organized way. Added code comments and also added a footer that provides Team Member info.

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import streamlit.components.v1 as components
 
+
 st.title(':snake: Pyrates :pirate_flag:')
 st.header('Welcome to the Pyrates Project!')
 
@@ -9,6 +10,8 @@ st.header('Welcome to the Pyrates Project!')
 country_codes = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/country_codes.csv")
 country_indicators = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/country_indicators.csv")
 pirate_attacks = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/pirate_attacks.csv")
+
+#BUGFIX for getting country code using country name. Code by SG.
 temp = country_codes
 country_dict = temp.set_index('country_name').T.to_dict('list')
 
@@ -31,16 +34,38 @@ def select_by_country(df):
     country = st.selectbox('Select A Country To View Pirate Attacks for that Country:', country_codes['country_name'])
     st.write("You selected:", df.loc[df['nearest_country'] == country_dict[country][0]])
 
+#EXPERIMENTAL GRAPH CHARTS. CODE BY SG
 def plot_attacks(df):
     plot_type = st.selectbox("Select A Category To Plot Number of Attacks by:", pirate_attacks.columns)
     #st.write("You selected:", plot_type)
     data = pd.DataFrame(df.value_counts(plot_type))
    # st.write(data)
     st.bar_chart(data)
- 
-# Menu code that generates menu options that allows user to aggerate data based category. Code by BG
-option = st.selectbox('Select how you would like to sort the data: ', ('Select', 'Country', 'Year', 'Attack Type'))
 
+#EXPERIMENTAL - TABBED CONTAINER - Code by BG
+tab1, tab2, tab3 = st.tabs(["Categorized Lists", "Graphical Data[BETA]", "Attack Prediction Score[BETA]"])    
+ 
+with tab1:
+    st.text('DESCRIPTION')
+    # Menu code that generates menu options that allows user to aggerate data based category. Code by BG
+    option = st.selectbox('Select how you would like to sort the data: ', ('Select', 'Country', 'Year', 'Attack Type'))
+
+with tab2:
+    st.text('DESCRIPTION')
+    plot_attacks(pirate_attacks)
+
+with tab3:
+    st.text('DESCRIPTION')
+    latitude = st.text_input(
+        'Enter Your Latitude:',
+        placeholder = 'Latitude'
+    )
+
+    longtitude = st.text_input(
+        'Enter Your Longtitude:',
+        placeholder = 'Longtitude'
+    )
+    st.button('Generate Your Risk Score!')
 if option is 'Country':
     select_by_country(pirate_attacks)
 
@@ -49,23 +74,22 @@ if option is 'Year':
 
 if option is 'Attack Type':
     select_by_attack(pirate_attacks)
-    plot_attacks(pirate_attacks)
+    #plot_attacks(pirate_attacks)
+
+# Menu code that generates menu options that allows user to aggerate data based category. Code by BG
+#option = st.selectbox('Select how you would like to sort the data: ', ('Select', 'Country', 'Year', 'Attack Type'))
+
+#if option is 'Country':
+#    select_by_country(pirate_attacks)
+
+#if option is 'Year':
+ #   select_by_year(pirate_attacks)
+
+#if option is 'Attack Type':
+ #   select_by_attack(pirate_attacks)
+ #   plot_attacks(pirate_attacks)
 
 st.divider()
+st.text('App Created @ HackNJIT 2023 by team Pyrates.')
+st.text('Pyrates Team Members: Amrit Madabushi, Brandon Green, Dan Farrell and Saketh G. ')
 
-#col1, col2, col3 , col4, col5 = st.beta_columns(5)
-
-#with col1:
- #   pass
-#with col2:
- #   pass
-#with col4:
-#    pass
-#with col5:
- #   pass
-#with col3 :
-   # center_button = st.button('Check Your Ship\'s Risk Score!')
-
-st.markdown('<center>', unsafe_allow_html=True)
-st.button('Check Your Ship\'s Risk Score!')
-st.markdown('</center>', unsafe_allow_html=True)
