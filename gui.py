a5b18c854ee6c968503df310db10381b205025e8
Amrit Madabushi <am2699@njit.edu>
2023-11-04 22:59:38 -0400

    Update gui.py
    Adds slider in a function

--- gui.py
+++ gui.py
@@ -35,12 +35,23 @@ def select_by_country(df):
 
 @st.cache_data
 def load_data(nrows):
+    DATE_COLUMN = 'date'
+    DATA_URL = 'https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/pirate_attacks.csv'
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
+
+def map_show(data):
+    DATE_COLUMN = 'date'
+    DATA_URL = 'https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/pirate_attacks.csv'
+    date_to_filter = st.slider('date', 1993, 2020, 1993)
+    filtered_data = data[data[DATE_COLUMN].dt.year == date_to_filter]
+    st.subheader(f'Map of all pirate attacks in {date_to_filter}')
+    st.map(filtered_data)
+    
 
 
 st.dataframe(country_codes)
@@ -60,15 +71,12 @@ select_by_attack(pirate_attacks)
 #calling the select by country function
 select_by_country(pirate_attacks)
 
-DATE_COLUMN = 'date'
-DATA_URL = 'https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/pirate_attacks.csv'
+
 
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
+data = load_data(9000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache_data)")
-
-st.subheader('Map of all pirate attacks')
-st.map(data)
+map_show(data)
