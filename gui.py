281758dc6fc57990cc1b87ea2caef46e9b28a41a
programmerBrandon <100105534+programmerBrandon@users.noreply.github.com>
2023-11-04 20:04:10 -0400

    Update gui.py - Reformatted Code, code structure nearly done.
    Just have to integrate bug fix for Country selection and primary functions will be done.

--- gui.py
+++ gui.py
@@ -6,8 +6,10 @@ import streamlit.components.v1 as components
 country_codes = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/country_codes.csv")
 country_indicators = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/country_indicators.csv")
 pirate_attacks = pd.read_csv("https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/pirate_attacks.csv")
+        
 
-# Helper function for select__by_years() function.
+
+# Helper function for select__by_years() function. 
 def years_only(x):
     return x.split('-')[0]
 
@@ -17,7 +19,7 @@ def select_by_year(df):
     year = st.selectbox('Select A Year To View Pirate Attacks for that Year:', df["date"].unique())
     st.write('You selected:', df.loc[df['date'] == year])
 
-# Function that creates selectbox that allows user to get attacks by type. Code by SG & BG. 
+# Function that creates selectbox that allows user to get attacks by type. Code by SG & BG.
 def select_by_attack(df):
     attack = st.selectbox('Select An Attack Type to View Attacks of that Type:', df["attack_type"].dropna().unique())
     st.write('You selected:', df.loc[df['attack_type'] == attack])
@@ -31,6 +33,18 @@ option = st.selectbox(
     'Select A Country To view Info:',
     (cnames))
 
+''' Function that generates the options for sorting the data. Code by BG'''
+    
+
 ##### MAIN DRIVER CODE ###
-select_by_year(pirate_attacks)
-select_by_attack(pirate_attacks)
+# Menu code that generates menu options that allows user to aggerate data based category. 
+option = st.selectbox('Select how you would like to sort the data: ', ('', 'Country', 'Year', 'Attack Type'))
+
+if option is 'Country':
+    pass
+
+if option is 'Year':
+    select_by_year(pirate_attacks)
+
+if option is 'Attack Type':
+    select_by_attack(pirate_attacks)
