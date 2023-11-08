0008b360b04207f1b40b29f290fc3986c9f3f1ef
programmerBrandon <100105534+programmerBrandon@users.noreply.github.com>
2023-11-07 22:57:41 -0500

    Update gui.py - Turning off map cache temporarily to try and test map bug

--- gui.py
+++ gui.py
@@ -50,7 +50,7 @@ def plot_attacks(df):
     st.bar_chart(data, x=plot_type, y="count")
 
 #Map Of Pirate Attack Locations - Code by AM
-@st.cache_data
+#@st.cache_data
 def load_data(nrows):
     DATE_COLUMN = 'date'
     DATA_URL = 'https://raw.githubusercontent.com/FanDarrell/HackNJIT_ProjectPyrates/main/pirate_attacks.csv'
